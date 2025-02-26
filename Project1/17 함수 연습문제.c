c84a2918b6cb540ff35e9aa81a9757f142400163
defect <gimm9457@gmail.com>
2025-02-27 01:22:55 +0900

    과제
    과제

--- Project1/17 함수 연습문제.c
+++ Project1/17 함수 연습문제.c
@@ -2,6 +2,79 @@
 #include <stdarg.h>//가변 인자 제공
 #include <math.h> //수학 기능 제공
 
+//1번 문제
+void Quadformula(double a, double b, double c)
+{
+	double d = b * b - 4 * a * c;
+	double result = -1 * b / 2 / a + sqrt(d) / 2 * a;
+	double result2 = -1 * b / 2 / a - sqrt(d) / 2 * a;
+
+	printf("x²- 2x = 2 의 근은 x = %f  ", result);
+	printf("또는 x = %f\n", result2);
+
+
+}
+
+//2번 문제
+void printrank(int a)
+{
+	if (a = 1)
+	{
+		printf("1st");
+	}
+	else if (a = 2)
+	{
+		printf("2nd");
+	}
+	else if (a = 3)
+	{
+		printf("3rd");
+	}
+	else (3 < a || a < 22);
+	{
+		printf("%dth", a);
+	}
+
+}
+
+//3번 문제
+float average(int count, ...)
+{
+	va_list args;
+	va_start(args, count);
+
+	float result = 0;
+
+	for (int i = 0; i < count; i++)
+	{
+		result += va_arg(args, int);
+	}
+
+	float y = result / count;
+
+	printf("\n");
+	va_end(args);
+
+	printf("평균값은 %.2f\n", y);
+
+	return y;
+
+}
+
+//4번 문제
+void isodd(int a)
+{
+	if (a % 2 == 0)
+	{
+		printf("NO\n");
+	}
+	else if (a % 2 != 0)
+	{
+		printf("YES\n");
+	}
+}
+
+
 
 int main()
 {
@@ -10,9 +83,16 @@ int main()
 	double a = 1;
 	double b = -2;
 	double c = -2;
-	Quadformula(a,b,c);
+	Quadformula(a, b, c);
 	//힌트 : 2차 방정식을 풀기 위해선 근의 공식을 이용해봅니다.
 
+	
+	
+	
+	
+	
+	
+	
 	//2. 다음 조건을 만족하는 printRank를 구현하세요.
 	int rank = 1;
 	printRank(rank); //1st
@@ -26,6 +106,7 @@ int main()
 	float result1 = average(3, 100, 95, 90);   //95.00
 	float result2 = average(4, 100, 95, 90, 80); //91.25
 	//힌트) printf("%.2f, value);를 활용하면 소수점 2자리까지 출력 가능
+
 
 	//4.다음 조건을 만족하는 isOdd를 구현하세요.
 	isOdd(3); //"YES"
